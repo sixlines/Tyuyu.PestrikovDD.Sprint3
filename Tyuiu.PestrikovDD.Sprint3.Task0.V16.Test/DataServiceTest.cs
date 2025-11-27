a4863403e94ef1445796425a1b775af3a11fcf2f
PestrikovDD <theuselessfox1@gmail.com>
2025-11-27 13:38:14 +0500

    Add project files.

---
+++ Tyuiu.PestrikovDD.Sprint3.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.PestrikovDD.Sprint3.Task0.V16.Lib;
+namespace Tyuiu.PestrikovDD.Sprint3.Task0.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int x = 5;
+            int startValue = 2;
+            int stopValue = 2;
+
+            double res = ds.GetMultiplySeries(x, startValue, stopValue);
+
+            double wait = Math.Pow(x / 2, 3);
+
+            Assert.AreEqual(res, wait);
+
+        }
+        [TestMethod]
+        public void ValidSetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            int x = 5;
+            int startValue = 1;
+            int stopValue = 3;
+
+            double res = ds.GetMultiplySeries(x, startValue, stopValue);
+
+            double wait = Math.Pow(x / 1, 3) * Math.Pow(x / 2, 3) * Math.Pow(x / 3, 3);
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.PestrikovDD.Sprint3.Task0.V16/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PestrikovDD.Sprint3.Task0.V16.Lib;
+
+namespace Tyuiu.PestrikovDD.Sprint3.Task0.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int startValue = 1;
+            int stopValue = 6;
+            Console.WriteLine(" Переменная x = " + x);
+            Console.WriteLine(" Старт шага = " + startValue);
+            Console.WriteLine(" Конец шага " + stopValue);
+            Console.WriteLine(" Произведение ряда: " + ds.GetMultiplySeries(x, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
